1835257498d7af275acfe5cc74a48288ae21f0dd
Rafał Wójcik <hefajstore@gmail.com>
2021-03-20 02:25:46 +0100

    Finish login screen design, add now main window

--- EOEOSystem/loginScreen.xaml.cs
+++ EOEOSystem/loginScreen.xaml.cs
@@ -1,28 +1,30 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows;
 
 namespace EOEOSystem
 {
-    /// <summary>
-    /// Logika interakcji dla klasy loginScreen.xaml
-    /// </summary>
     public partial class loginScreen : Window
     {
         public loginScreen()
         {
             InitializeComponent();
+        }
+
+        private void ExitBtn_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void LoginBtn_Click(object sender, RoutedEventArgs e)
+        {
+            // walidacja danych
+            var a = passwordTb.Text;
+            var b = loginTb.Text;
+
+
+            // jezeli sie powiedzie to otwiera main window
+            var mainScreen = new MainScreen();
+            mainScreen.Show();
+            this.Close();
         }
     }
 }
